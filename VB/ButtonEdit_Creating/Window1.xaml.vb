4d516164e4af770f531e284f9f77b28fba1f00a3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/ButtonEdit_Creating/Window1.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace ButtonEdit_Creating
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub ButtonInfo_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			buttonEdit.EditValue = String.Empty
+		End Sub
+	End Class
+End Namespace
